8f49b49191081c2499c7ed01c2747ffd9ce87f19
candyapplecorn <candyapplecorn@gmail.com>
2017-08-31 21:23:39 -0700

    js hw

---
+++ W5D5/timeouts.js
@@ -0,0 +1,10 @@
+window.setTimeout(function(){
+				alert('hammertime')
+}, 5000)
+
+function is_hammertime(time){
+				window.setTimeout(function(){
+								alert('hammertime')
+				}, time)
+}
+is_hammertime(1000)
