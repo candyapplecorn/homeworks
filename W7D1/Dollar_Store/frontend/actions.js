5ba4dfbbd690a28fa808b131d852ce2075f996ed
candyapplecorn <candyapplecorn@gmail.com>
2017-09-10 22:15:15 -0700

    completed dollar store - it is not one though...

---
+++ W7D1/Dollar_Store/frontend/actions.js
@@ -0,0 +1,7 @@
+const selectCurrency = (baseCurrency, rates) => ({
+    type: "SWITCH_CURRENCY",
+    baseCurrency,
+    rates
+});
+window.selectCurrency = selectCurrency
+export default selectCurrency;
---
+++ W7D1/Dollar_Store/frontend/reducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+  baseCurrency: "Please select",
+  rates: {}
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type){
+    case ("SWITCH_CURRENCY"):
+      const { baseCurrency, rates } = action
+      return { baseCurrency, rates };
+      break;
+    default:
+      return state; // remove this and fill out the body of the reducer function
+  }
+};
+
+window.reducer = reducer;
+export default reducer;
